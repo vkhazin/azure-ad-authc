727542047e11a240b341ed0e3a9a7efaf3a92a94
Vlad Khazin <vladimir.khazin@icssolutions.ca>
2019-12-10 01:18:45 +0000

    work in progress

---
+++ AuthN/Authenticate.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Security;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Identity.Client;
+
+namespace AzureADAuthenticationService
+{
+  //Response contract
+  struct Token
+  {
+      public string token_type;
+      public string access_token;
+      public string scope;
+      public string ext_expires_in;
+      public string refresh_token;
+      public string id_token;
+  }
+
+  public static class Authenticate
+  {
+    //Environment Variables
+    private static string ClientId = Environment.GetEnvironmentVariable("AppClientID");
+    private static string ClientSecret = Environment.GetEnvironmentVariable("ClientSecret");
+
+    /// <summary>
+    /// Azure function entry point
+    /// </summary>
+    /// <param name="username">username</param>
+    /// <param name="password">password</param>
+    /// <returns>Token</returns>
+    [FunctionName("Authenticate")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]
+        HttpRequest req, 
+        ILogger log)
+    {
+      try
+      {
+        log.LogInformation("Processing Authentication Request");
+
+        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        dynamic data = JsonConvert.DeserializeObject(requestBody);
+        string username = data?.username;
+        string password = data?.password;
+
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+        {
+            return new BadRequestObjectResult(new { Message = "Missing username and/or password" });
+        }
+
+        try
+        {
+            var token = await GenerateToken(username, password);
+            return new JsonResult(new { Token = token });
+        }
+        catch (MsalException)
+        {
+            return new BadRequestObjectResult(new { Message = "Invalid username and/or password" });
+        }
+      }
+      catch (Exception exception)
+      {
+          return new BadRequestObjectResult(new { exception.Message });
+      }
+    }
+
+
+    /// <summary>
+    /// Generate Token based on the passed Azure AD credentials
+    /// </summary>
+    /// <param name="username">username</param>
+    /// <param name="password">password</param>
+    /// <returns>Token</returns>
+    private static async Task<string> GenerateToken(string username, string password)
+    {
+    using (var client = new HttpClient())
+    {
+
+        client.BaseAddress = new Uri("https://login.microsoftonline.com");
+        var request = new HttpRequestMessage(HttpMethod.Post, "/organizations/oauth2/v2.0/token");
+
+        var keyValues = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("client_id", ClientId ),
+            new KeyValuePair<string, string>("scope", "user.read openid profile offline_access"),
+            new KeyValuePair<string, string>("client_secret",ClientSecret),
+            new KeyValuePair<string, string>("username", username),
+            new KeyValuePair<string, string>("password", password),
+            new KeyValuePair<string, string>("grant_type", "password")
+        };
+
+        request.Content = new FormUrlEncodedContent(keyValues);
+        var response = await client.SendAsync(request);
+
+        var content = JsonConvert.DeserializeObject<Token>(await response.Content.ReadAsStringAsync());
+
+        return string.IsNullOrEmpty(content.access_token) ? "empty token" : content.access_token;
+      }
+    }
+
+
+  }
+}
---
+++ readme.md
@@ -0,0 +1,102 @@
+# Azure AD Authentication Service
+
+## Authentication Function
+
+### Request
+
+* Verb: POST 
+* Route: `/`
+* Body:
+```
+{
+   "username": "azure-ad email address",
+   "password": "azure-ad password"
+}
+```
+
+### Response Success
+
+* Http Code: 200
+* Body:
+```
+{
+   "token": "token representing username/password identity"
+}
+```
+
+### Response Failure
+
+* Http Code: 401
+* Body:
+```
+{
+   "message": "Invalid username and/or password"
+}
+```
+
+## Setup on [Visual Studio Online](https://online.visualstudio.com/)
+
+* [Reference](https://docs.microsoft.com/en-us/azure/azure-functions/functions-create-first-azure-function-azure-cli)
+* Install [Azure Functions Core Tools](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local#v2):
+```
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-bionic-prod bionic main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools -y
+```
+* Login to az cli: `az login`
+* One time per Azure subscription configuration and resources:
+```
+namePrefix=authpoc
+nameResourceGroup=${namePrefix}-resource-group
+nameStorageAccount=${namePrefix}storage
+location=centralus
+az group create --name $nameResourceGroup --location $location
+az storage account create --name $nameStorageAccount --location $location --resource-group $nameResourceGroup
+```
+* Create function:
+```
+az functionapp create --resource-group $nameResourceGroup --consumption-plan-location $location \
+--name trgos-authentication --storage-account  $nameStorageAccount --runtime dotnet
+```
+
+## Development
+
+* Clone the repo
+* Modify the code
+* Publish the function from AuthN folder: `func azure functionapp publish trgos-authentication`
+* [Access function logs](https://markheath.net/post/three-ways-view-error-logs-azure-functions)
+* Advanced logging future [options](https://stackify.com/logging-azure-functions/)
+
+## Azure AD Setup
+
+* Proceed to Azure AD from Azure Portal
+* Register a new application: `Add your own application`
+* Select a `native/desktop application` instead of the default web application
+* Copy `Application Client ID` you will need it to configure the function variables
+* Select `View API Permissions`
+* Select `Grant Admin Consent`
+* There is a delay until consent is granting, wait for the green icon before proceeding
+* Select `Authentication` on the left hand menu
+<!-- * Scroll down to `Default client type`
+* Select `yes` for: `Treat application as a public client` -->
+* Select `Certificates & Secrets` from the left hand
+* Generate a new client secret and save it in a save place - you won't see it ever again
+
+## Function Configuration
+
+* In order to authenticate against active directory, client id and secret will be required
+* These two are stored in env variables
+* To configure the environment variables for existing function:
+```
+az functionapp config appsettings set --name trgos-authentication --resource-group authpoc-resource-group \
+   --settings "AppClientID=--app client id--" \
+   --settings "ClientSecret=--client-secret--"
+```
+
+## To test the deployment
+
+* Create a user or two using Azure AD Portal
+* 
+
